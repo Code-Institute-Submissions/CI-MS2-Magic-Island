941355fc72531b372d1104ebcb8d4618d5f7962a
Henriqueperoni <henriqueperoni@hotmail.com>
2020-10-21 19:33:19 +0000

    Create for loop to close burguer navbar in small screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const navSlide = () => {
   const burguer = document.querySelector(".burguer");
   const nav = document.querySelector(".nav-links");
+  const navClose = document.querySelectorAll(".nav-anchor");
 
   burguer.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
   });
+
+  for (i = 0; i < navClose.length; i += 1) {
+    navClose[i].addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+    });
+  }
 };
 
 navSlide();
@@ -15,4 +22,9 @@ navSlide();
 //   x: "-110vw",
 //   background: "-webkit-radial-gradient(center, #FFEF47, #FFA948)",
 //   duration: 25,
+// });
+
+// document.querySelector(".nav-anchor").addEventListener("click", () => {
+//   document.querySelector("a.nav-anchor").classList.add("active");
+//   //   document.querySelector("a.nav-anchor").classList.add("active");
 // });
