0e17eb7bcbb1c64827246d87caf6ea60723d74b0
Henriqueperoni <henriqueperoni@hotmail.com>
2020-10-21 20:08:47 +0000

    Add animation when load the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,14 @@ const navSlide = () => {
 
 navSlide();
 
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".intro-beach", { x: "0%", duration: 1, delay: 3 });
+tl.to(".intro-activities", { x: "0%", duration: 1, delay: 2 });
+tl.to(".slider", { y: "-100%", duration: 1, delay: 2 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1.1");
+tl.fromTo("nav, .hero-text", { opacity: 0 }, { opacity: 1, duration: 1 });
+
 // const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 // tl.from(".sun", {
