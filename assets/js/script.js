893c87579286475497b5c5074acefdf4e640a12b
Henriqueperoni <henriqueperoni@hotmail.com>
2020-10-21 09:17:15 +0000

    Add responsive navigation bar for small screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+const navSlide = () => {
+  const burguer = document.querySelector(".burguer");
+  const nav = document.querySelector(".nav-links");
+
+  burguer.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
+
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.from(".sun", {
