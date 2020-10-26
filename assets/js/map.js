d821910cee1e8a1fa23a46e6d79e1e16a2fe67a2
Henriqueperoni <henriqueperoni@hotmail.com>
2020-10-26 11:24:38 +0000

    Add markers to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,81 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
   const mallorca = { lat: 39.617909, lng: 2.978694 };
-  // The map, centered at Uluru
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: mallorca,
   });
-  // The marker, positioned at Uluru
+  var markers = [
+    {
+      coords: { lat: 39.31361, lng: 3.121398 },
+      content: "<h2>Caló des Moro</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.720876, lng: 3.454833 },
+      content: "<h2>Calla Agulla</h2>",
+    },
+    {
+      coords: { lat: 39.330299, lng: 3.146721 },
+      content: "<h2>Cala Santanyí</h2>",
+    },
+    {
+      coords: { lat: 39.329965, lng: 3.171178 },
+      content: "<h2>Restaurant L'Arcada</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+    {
+      coords: { lat: 39.349545, lng: 3.185803 },
+      content: "<h2>Playa de S'Amarador</h2>",
+    },
+  ];
+
+  for (var i = 0; i < markers.length; i++) {
+    // Add marker
+    addMarker(markers[i]);
+  }
+
+  function addMarker(props) {
+    let marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+    });
+
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content,
+      });
+    }
+
+    marker.addListener("click", function () {
+      infoWindow.open(map, marker);
+    });
+  }
 }
